cf09b8d624263125c903632f6c074fb936d6c0d5
chick_0 <chick_0@ch1ck.xyz>
2022-05-03 03:27:17 +0900

    add project create

--- app/views/project.py
+++ app/views/project.py
@@ -1,8 +1,10 @@
+from uuid import uuid4
 from datetime import datetime
 
 from flask import Blueprint
 from flask import request
 from flask import jsonify
+from sqlalchemy.exc import IntegrityError
 
 from app import db
 from app.models import Project
@@ -96,11 +98,58 @@ def edit_project(project_id: str, payload: dict):
     except KeyError:
         return error(
             code=400,
-            message="프로젝트를 수정 할 수 없습니다."
+            message="프로젝트를 저장 할 수 없습니다."
         )
 
     db.session.commit()
 
     return jsonify({
         "status": True
+    })
+
+
+@bp.post("")
+@login_required
+def create_project(payload: dict):
+    pj = Project()
+    pj.uuid = uuid4().__str__()
+
+    json = request.get_json(silent=True)
+    if json is None:
+        return error(
+            code=400,
+            message="요청이 올바르지 않습니다."
+        )
+
+    try:
+        pj.title = json['title']
+
+        pj.date = datetime.strptime(json['dt'], "%Y-%m-%d")
+
+        pj.tag = json['tags']
+
+        pj.web = json['web']
+        pj.github = json['github']
+
+        pj.a = json['content']['a']
+        pj.b = json['content']['b']
+        pj.c = json['content']['c']
+    except KeyError:
+        return error(
+            code=400,
+            message="프로젝트를 저장 할 수 없습니다."
+        )
+
+    try:
+        db.session.add(pj)
+        db.session.commit()
+    except IntegrityError:
+        return error(
+            code=400,
+            message="[UUID 중복] 프로젝트 저장 버튼을 다시 눌러주세요."
+        )
+
+    return jsonify({
+        "status": True,
+        "uuid": pj.uuid
     })
