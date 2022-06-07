5cfe274c0ed00536bc562873d88c767a6bc7049f
chick_0 <chick_0@ch1ck.xyz>
2022-06-07 18:28:49 +0900

    create project list

--- v2/projects/index.py
+++ v2/projects/index.py
@@ -1,21 +1,72 @@
+from math import ceil
+
 from fastapi import APIRouter
+from fastapi import HTTPException
+from pydantic import BaseModel
 
-router = APIRouter(
-    prefix="/index"
-)
+from sql import get_session
+from sql.models import Project
+
+router = APIRouter()
+
+ITEM_PER_PAGE = 20
+
+
+class ProjectPreview(BaseModel):
+    uuid: str
+    title: str
+    date: int
+    tags: list[str]
+
+
+class PageData(BaseModel):
+    this: int
+    max: int
+
+
+class ProjectList(BaseModel):
+    projectList: list[ProjectPreview]
+    page: PageData
 
 
 @router.get(
-    "",
-    description="프로젝트 목록을 불러옵니다."
+    "/",
+    description="프로젝트 목록을 불러옵니다.",
+    response_model=ProjectList
 )
-async def show_list(page_id: int):
-    return []
+async def show_list(page: int = 1):
+    session = get_session()
 
+    length = session.query(Project).count()
+    max_page = ceil(length / ITEM_PER_PAGE)
 
-@router.get(
-    "/tags",
-    description="태그와 관련된 프로젝트 목록을 불러옵니다."
-)
-async def show_list_with_tags(tags: str, page_id: int):
-    return []
+    if page > max_page or page <= 0:
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "alert": "요청하신 페이지를 찾을 수 없습니다.",
+                "max": max_page
+            }
+        )
+
+    return ProjectList(
+        projectList=[
+            ProjectPreview(
+                uuid=x.uuid,
+                title=x.title,
+                date=round(x.date.timestamp()),
+                tags=[x.strip() for x in x.tag.split(",")]
+            ) for x in session.query(Project).order_by(
+                Project.date.desc()
+            ).with_entities(
+                Project.uuid,
+                Project.title,
+                Project.date,
+                Project.tag,
+            ).offset(ITEM_PER_PAGE * (page - 1)).limit(ITEM_PER_PAGE).all()
+        ],
+        page=PageData(
+            this=page,
+            max=max_page,
+        )
+    )
