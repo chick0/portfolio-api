a741c8dcc3e15d46d09e7d37663d403593d77f65
chick_0 <chick_0@ch1ck.xyz>
2022-06-09 01:14:54 +0900

    create token check endpoint

--- v2/auth/__init__.py
+++ v2/auth/__init__.py
@@ -1,4 +1,5 @@
 __all__ = [
+    "check",
     "history",
     "login",
     "logout",
---
+++ v2/auth/check.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from fastapi import APIRouter
+from fastapi import HTTPException
+from fastapi import Depends
+from fastapi.security import HTTPBearer
+from pydantic import BaseModel
+
+from v2.utils import parse_token
+
+router = APIRouter(
+    prefix="/check"
+)
+
+auth_scheme = HTTPBearer()
+
+
+class TokenCheckResponse(BaseModel):
+    status: bool
+    ttl: int
+
+
+@router.get(
+    "",
+    description="인증 토큰이 유요한지 검사합니다.",
+    response_model=TokenCheckResponse
+)
+async def check_token(token=Depends(auth_scheme)):
+    try:
+        payload = parse_token(token=token)
+        ttl = (datetime.now() - datetime.fromtimestamp(payload.exp)).seconds
+    except HTTPException:
+        payload = None
+        ttl = 0
+
+    return TokenCheckResponse(
+        status=payload is not None,
+        ttl=ttl
+    )
